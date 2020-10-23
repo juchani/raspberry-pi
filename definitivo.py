11e5b3a8b012e6d87b6281d3b424326a4d810fef
juchani <victorfrancojuchani@gmail.com>
2020-10-23 16:30:55 -0400

    Update definitivo.py

--- definitivo.py
+++ definitivo.py
@@ -5,17 +5,13 @@ import yagmail
 camara = PiCamera()
 pir = MotionSensor(4)
 
-
-
-
-
 while 1:
     print("Sensor pir {}".format(pir.value))
     if pir.value:
         camara.capture('/home/pi/s.jpg')
         try:
             yag = yagmail.SMTP(user='victorfrancojuchani@gmail.com', password='zauwoczkofpfbsvt')
-            yag.send(to='victorfrancojuchani@gmail.com', subject='Alarma Activada', contents='El sensor detecto movimiento,Temperatura {}*C Humedad {}%'.format(temperatura, humedad), attachments='/home/pi/s.jpg')
+            yag.send(to='victorfrancojuchani@gmail.com', subject='Alarma Activada', contents='El sensor detecto movimiento', attachments='/home/pi/s.jpg')
             print("El email fue enviado")
         except:
             print("Error, El correo no fue enviado")    
